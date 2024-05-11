2512b3fba5d5d673856af65d29cf25cd64c7706e
annejscott <157659040+annejscott@users.noreply.github.com>
2024-05-11 13:08:47 -0400

    initial commit

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='cookiecutter gh:drivendata/cookiecutter-data-science',
-    author='AJ Strauman-Scott',
-    license='MIT',
+    version="0.1.0",
+    description="Analyze Level 3 stop-and-frisk data from recent years by racial, gender and location markers",
+    author="AJ Strauman-Scott",
+    license="MIT",
 )
